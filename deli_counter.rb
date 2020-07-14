f6f1d063c7fa55ceaa1bfd9266d9749393c874d4
Simba Mumanyi <simbamumanyi@gmail.com>
2020-07-14 17:58:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(x)
    x.each.with_index(1) do  |name, index|
      line_array.push ("#{index}. #{name}")
   end
-  puts "The line is currently #{"
+  puts "The line is currently #{line_array.join"
 
   
   
